9074c9ff80a0cb330121dcd6a3101f4867a568a2
Francis G-Tessier <francis.gtessier@gmail.com>
2015-03-17 20:51:24 -0400

    hide shape links base on role

---
+++ Lab5/Extensions/HtmlExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab5.Extensions
+{
+    public static class HtmlExtensions
+    {
+        /// <summary>
+        /// This method returns output when evaluation criteria is meet
+        /// </summary>
+        /// <param name="value">Html Helper</param>
+        /// <param name="evaluation">Evaluation criteria</param>
+        /// <returns>Html output</returns>
+        public static MvcHtmlString If(this MvcHtmlString value, bool evaluation)
+        {
+            return evaluation ? value : MvcHtmlString.Empty;
+        }
+    }
+}
